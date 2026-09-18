2690bc82ef5e01dc19a526c0d338847511ab69c7
Alumno Mañana <Alumno Maana@LB-DAM-21.iesluisbraille.es>
2026-09-18 14:07:10 +0200

    Actualizacion_Mansion_Zombi

--- Mansion_Zombie/src/Zombie.java
+++ Mansion_Zombie/src/Zombie.java
@@ -1,4 +1,7 @@
-public class Zombie {
+import java.util.ArrayList;
 
-    int Ataque_Zombie = (int) (Math.random()) + 2;
+public class Zombie {
+    ArrayList<Atributos_Mansion_Zombi> Num_Habitacion = new ArrayList<>();
+
+    int Ataque_Zombie = (int) (Math.random() * 1) + 2 + Num_Habitacion.get(0).getNumHabitacion();
 }
